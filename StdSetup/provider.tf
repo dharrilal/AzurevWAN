ec170fe1cbb228338ada5989ed78c0cf0698d0b1
dharrilal <denesh.harrilal@gmail.com>
2023-02-22 13:03:55 -0400

    updated provider

--- StdSetup/provider.tf
+++ StdSetup/provider.tf
@@ -18,11 +18,17 @@ provider "azurerm" {
 }
 provider "azurerm" {
   alias           = "hub-sub"
+  client_id       = "bec8538f-c245-4e2b-8f83-e2e372915ee9"
+  client_secret   = "Cub8Q~YCd4UJvz~CcAR73NBhuwbU8O0oS58k.bzQ"
+  tenant_id       = "c2129b89-e528-4ed6-93dc-8ec4b9b7304f"
   subscription_id = var.subscription_id_hub
   features {}
 }
 provider "azurerm" {
   alias           = "spoke-sub"
+  client_id       = "37f800a4-5385-4531-9adb-ed933cbd92a1"
+  client_secret   = "iWn8Q~4UlUKwvReOYi-gdYHdT4pSyfB7Lieflb8E"
+  tenant_id       = "c2129b89-e528-4ed6-93dc-8ec4b9b7304f"
   subscription_id = var.subscription_id_spoke
   features {}
 }
